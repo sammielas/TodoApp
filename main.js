dacb3b572a76c96b89695a5696a17143266d9913
Samson Lasode <samflicks@gmail.com>
2019-06-12 11:08:33 +0100

    Added the Delete Option, renamed todo to todos and used a foreach to iterate

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 
-let todo= ["Buy Kilishi"];
+let todos= ["Buy new Bag"];
+
 let input= prompt("Enter Your Choice");
 
- while(input!== "QUIT"){
-  if(input==="LIST"){
-    console.log(todo)
-  }else if(input==="NEW"){
+ while(input!== "quit"){
+   //Handle Input
+  if(input === "list"){
+    console.log("**********");
+    todos.forEach(function(todo, i){
+      console.log(i +": " + todo);
+    });
+   console.log("**********");
+  } else if(input==="new"){
+    //Ask for new Todo
   let newTodo= prompt("Add new todo");
-      todo.push(newTodo);
+  //add to todos Array
+      todos.push(newTodo);
   }
-  
-    input= prompt("Enter Your Choice");
+  else if(input==="delete"){
+   //Ask for index of item to be deleted
+   let choice=prompt("Are you sure you want to delete? Enter \"yes\" if sure, else enter \"no\"");
+   if(choice ==="yes"){
+    let index= prompt("Please enter index of item to be deleted")
+    //delete item
+     todos.splice(index,1);
+   }
+   
+  }
+   //ask again for new input
+    input= prompt("What else would you like to do?");
   
  }
  console.log("You have chosen to end the program");
